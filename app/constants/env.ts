da98be9c49fe64e5ba0818447962211d9dd0860e
Andre Bahtiar Fauzi <supersandre@gmail.com>
2022-02-07 22:51:10 +0700

    auto generate ns

--- app/constants/env.ts
+++
@@ -1,2 +0,0 @@
-const BASE_URL = 'https://quran.kemenag.go.id/index.php/api'
-export { BASE_URL }
--- app/utils/api.ts
+++
@@ -1,4 +0,0 @@
-import { BASE_URL } from '../constants/env'
-
-export const quranDetail = (surahId, jmlAyat) =>
-  `${BASE_URL}/v1/ayatweb/${surahId}/0/0/${jmlAyat}`
